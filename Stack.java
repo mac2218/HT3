14cafb0cd498e9709e0261715431a41ad3acc853
mac2218 <mac22118@uvg.edu.gt>
2023-02-20 20:04:52 -0600

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -22,3 +22,44 @@ def infix_to_postfix(infix_expr):
     op_stack = Stack()
     postfix_list = []
     token_list = infix_expr.split()
+
+    for token in token_list:
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfix_list.append(token)
+        elif token == '(':
+            op_stack.push(token)
+        elif token == ')':
+            top_token = op_stack.pop()
+            while top_token != '(':
+                postfix_list.append(top_token)
+                top_token = op_stack.pop()
+        else:
+            while (not op_stack.is_empty()) and (prec[op_stack.peek()] >= prec[token]):
+                postfix_list.append(op_stack.pop())
+            op_stack.push(token)
+
+    while not op_stack.is_empty():
+        postfix_list.append(op_stack.pop())
+
+    return " ".join(postfix_list)
+
+def postfix_to_infix(postfix_expr):
+    op_stack = Stack()
+    token_list = postfix_expr.split()
+
+    for token in token_list:
+        if token in "+-*/^":
+            operand2 = op_stack.pop()
+            operand1 = op_stack.pop()
+            infix_expr = "({} {} {})".format(operand1, token, operand2)
+            op_stack.push(infix_expr)
+        else:
+            op_stack.push(token)
+
+    return op_stack.pop()
+
+infix_expr = "A + B * C + D"
+postfix_expr = infix_to_postfix(infix_expr)
+print("Postfix: ", postfix_expr)
+infix_expr = postfix_to_infix(postfix_expr)
+print("Infix: ", infix_expr)
